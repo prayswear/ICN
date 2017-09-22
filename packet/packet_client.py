30d84ee783a01157ba2877ed0024e891bc887683
prayswear <475212995@qq.com>
2017-09-22 10:55:06 +0800

    bug fixed

--- packet/packet_client.py
+++ packet/packet_client.py
@@ -8,12 +8,12 @@ logger = logging.getLogger('myLogger')
 BUFFER_SIZE = 1024
 
 
-def send_packet(packet):
+def send_packet(dst_ip, dsp_port, packet):
     packet_size = len(packet)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        sock.connect((packet_server_ip, packet_server_port))
-        logger.info("Connect to server " + packet_server_ip + ":" + str(packet_server_port))
+        sock.connect((dst_ip, dsp_port))
+        logger.info("Connect to server " + dst_ip + ":" + str(dsp_port))
         sock.send(str(packet_size).encode("utf-8"))
         logger.info("Request: " + str(packet_size) + " has been sent.")
         reply = sock.recv(BUFFER_SIZE).decode("utf-8")
@@ -35,16 +35,28 @@ def send_packet(packet):
         logger.info('Socket: ' + str(sockname) + ' has been closed.')
 
 
+def lanjun_func(src_guid, dst_guid, src_na, dst_na, service_type, payload):
+    packet = ICNPacket()
+    packet.setHeader(src_guid, dst_guid, src_na, dst_na, service_type)
+    packet.setPayload(payload)
+    packet.fill_packet()
+    data = packet.grap_packet()
+    return data
+
+
 if __name__ == '__main__':
+    print(binascii.a2b_hex('ee'))
+    print(binascii.a2b_hex(b'ee'))
     packet_server_ip = '127.0.0.1'
     packet_server_port = 35000
     # data = b'\x01#Eg\x89\xab\xcd\xef\x01#Eg\x89\xab\xcd\xef\x12\xab\xcd\xef\xaf\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfa\x87eC!\x124Vx\xee7%\x16\xab\xcd\xef\xdc\xba'
-    packet=ICNPacket()
-    packet.setHeader('0123456789abcdef0123456789abcdef12abcdef','affffffffffffffffffffffffffffffffffffffa','87654321','12345678','11')
-    packet.setPayload(binascii.a2b_hex('e'*346536))
+    packet = ICNPacket()
+    packet.setHeader('0123456789abcdef0123456789abcdef12abcdef', 'affffffffffffffffffffffffffffffffffffffa', '87654321',
+                     '12345678', '11')
+    packet.setPayload(binascii.a2b_hex('e' * 346536))
     print(packet.payload)
     packet.fill_packet()
-    data=packet.grap_packet()
+    data = packet.grap_packet()
     packet.print_packet()
     print(len(data))
-    send_packet(data)
+    send_packet(packet_server_ip, packet_server_port, data)
