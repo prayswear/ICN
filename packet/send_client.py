2753532f09bf283127ff3f199a4866b894c0fa2d
prayswear <475212995@qq.com>
2017-10-18 16:27:31 +0800

    fix bugs

--- packet/send_client.py
+++ packet/send_client.py
@@ -25,14 +25,14 @@ def send_cmd_packet(data, address):
         return False
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.sendto(data, address)
-    # sock.settimeout(0.5)
-    # try:
-    #     reply, remote_addr = sock.recvfrom(1024)
-    #     logger.info(reply.decode('utf-8'))
-    # except socket.timeout:
-    #     logger.warning('Timeout and no reply')
-    # finally:
-    #     sock.close()
+    sock.settimeout(0.5)
+    try:
+        reply, remote_addr = sock.recvfrom(1024)
+        logger.info(reply.decode('utf-8'))
+    except socket.timeout:
+        logger.warning('Timeout and no reply')
+    finally:
+        sock.close()
     sock.close()
 
 
@@ -63,7 +63,9 @@ def send_data_packet(data, address):
             count += 1
             icn_packet.fill_packet()
             icn_packet.setPayload(icn_type+send_data)
-            sock.sendto(icn_packet.grap_packet, address)
+            sock.sendto(icn_packet.grap_packet(), address)
+            if count==1:
+                time.sleep(0.5)
         send_times -= 1
     sock.close()
 
@@ -87,24 +89,73 @@ def query_gnrs(euid):
         sock.close()
         return result
 
+def block4ip(euid):
+    result=None
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0.5)
+    s.bind(('0.0.0.0', 12321))
+    flag = 1
+    while flag:
+        try:
+            addr = ('0.0.0.0', 12321)
+            s.bind(addr)
+            query_result, gnrs_addr = s.recvfrom(1024)
+            a = ICNPacket()
+            a.gen_from_hex(query_result)
+            ppp = binascii.b2a_hex(a.payload).decode('utf-8')
+            if ppp[0:2] == '04' and ppp[2:34] == euid:
+                flag = 0
+                result=socket.inet_ntoa(ppp[34:42])
+        except socket.timeout:
+            print('***gnrs timeout retry***')
+        finally:
+            if flag == 0:
+                s.close()
+        return result
+
 
 if __name__ == '__main__':
     cmd_port = 35000
     data_port = 36000
     gnrs_addr = ('192.168.1.100', 8899)
     cam_euid = 'ab92b7014f2887ea05450143f4c9ad01'
-    query_packet = ICNPacket()
-    query_packet.setHeader('bf77ac7196110678d84d03687eab03fb', 'b13ded0762217339428aba5d508ddf5c', '00')
-    query_packet.setPayload(binascii.a2b_hex('03' + cam_euid + '00'))
-    query_packet.fill_packet()
-    query_packet.print_packet()
-    send_cmd_packet(query_packet.grap_packet(), (gnrs_addr))
-    cam_addr = (query_gnrs(cam_euid), cmd_port)
-    if not cam_addr[0] == None:
-        request_packet = ICNPacket()
-        request_packet.setHeader('bf77ac7196110678d84d03687eab03fb', 'b13ded0762217339428aba5d508ddf5c', '00')
-        request_packet.setPayload(binascii.a2b_hex('0d' + cam_euid))
-        request_packet.print_packet()
-        send_cmd_packet(request_packet.grap_packet(), cam_addr)
-    else:
-        logger.error('can not query GNRS!')
+    # query_packet = ICNPacket()
+    # query_packet.setHeader('bf77ac7196110678d84d03687eab03fb', 'b13ded0762217339428aba5d508ddf5c', '00')
+    # query_packet.setPayload(binascii.a2b_hex('03' + cam_euid + '00'))
+    # query_packet.fill_packet()
+    # query_packet.print_packet()
+    # send_cmd_packet(query_packet.grap_packet(), (gnrs_addr))
+    #cam_addr = (query_gnrs(cam_euid), cmd_port)
+
+    cam_addr = ('192.168.1.21', cmd_port)
+
+    while True:
+        notify_address=('0.0.0.0',23333)
+        notify_socket=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        notify_socket.bind(notify_address)
+        time.sleep(2)
+        #notify_data,notify_addr=notify_socket.recvfrom(1024)
+        print('recv notify reply')
+        # with open('delayflag.txt','w') as fp:
+        #     fp.write(str(time.time()))
+        #     fp.flush()
+        #     fp.close()
+        #send_cmd_packet(query_data, (gnrs_addr))
+        if not cam_addr == None:
+            request_packet = ICNPacket()
+            request_packet.setHeader('bf77ac7196110678d84d03687eab03fb', 'b13ded0762217339428aba5d508ddf5c', '00')
+            request_packet.setPayload(binascii.a2b_hex('0dab92b7014f2887ea05450143f4c9ad01'))
+            request_packet.fill_packet()
+            request_data = request_packet.grap_packet()
+            request_packet.print_packet()
+            print(len(request_data))
+            send_cmd_packet(request_data, cam_addr)
+
+    # if not cam_addr[0] == None:
+    #     request_packet = ICNPacket()
+    #     request_packet.setHeader('bf77ac7196110678d84d03687eab03fb', 'b13ded0762217339428aba5d508ddf5c', '00')
+    #     request_packet.setPayload(binascii.a2b_hex('0d' + cam_euid))
+    #     request_packet.print_packet()
+    #     send_cmd_packet(request_packet.grap_packet(), cam_addr)
+    # else:
+    #     logger.error('can not query GNRS!')
--- packet/test_server.py
+++ packet/test_server.py
@@ -36,21 +36,18 @@ def packet_recv_rate():
             time.sleep(2)
 
 
-def data_finish_handler(packet_id, address):
+def data_finish_handler(p,data, address):
+    icn_type=binascii.a2b_hex('09')
     logger.info('Recieve a packet from ' + str(address))
-    #global ave_packet_recv_rate
-    #total = temp_packet_dict[packet_id]['total']
-    #ave_packet_recv_rate = (total - temp_packet_dict[packet_id]['count']) / total
-    p = temp_packet_dict[packet_id]['icn_head']
-    p.setTLV = binascii.a2b_hex('')
-    p.payload = p.payload[:2] + temp_packet_dict[packet_id]['data']
+    content_euid = binascii.b2a_hex(p.payload)[2:34]
+    p.setPayload(icn_type+p.payload[:34]+data)
     p.print_packet()
-    del temp_packet_dict[packet_id]
+    reply_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    reply_socket.sendto('OK'.encode('utf-8'), address)
     # reply_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # reply_socket.sendto('OK'.encode('utf-8'), address)
-    content_euid = binascii.b2a_hex(p.payload)[2:34]
     with open('input.h264', 'wb') as f:
-        f.write(p.payload)
+        f.write(data)
         f.close()
     os.system('avconv -r 24 -i input.h264 -vcodec copy output.mp4 -y')
     filetime = str(int(time.time()))
@@ -62,57 +59,59 @@ def data_finish_handler(packet_id, address):
 def timeout_handler(packet_id, address):
     start_time = time.time()
     while True:
-        time_gap = time.time() - start_time
-        if time_gap > 2:
+        timegap = time.time() - start_time
+        if timegap > 2:
             if packet_id in temp_packet_dict.keys():
-                data_finish_handler(packet_id, address)
+                data_finish_handler(temp_packet_dict[packet_id]['icn_head'],temp_packet_dict[packet_id]['data'], address)
+                global ave_packet_recv_rate
+                total = temp_packet_dict[packet_id]['total']
+                ave_packet_recv_rate = (total - temp_packet_dict[packet_id]['count']) / total
+                del temp_packet_dict[packet_id]
             break
 
 
 def data_handler(data, address):
-    top_n = 10
     logger.info('Recv UDP packet from: ' + str(address))
     p = ICNPacket()
     p.gen_from_hex(data)
     packet_id = binascii.b2a_hex(p.tlv[0:2])
     packet_length = int(binascii.b2a_hex(p.tlv[2:6]), 16)
     packet_seq = int(binascii.b2a_hex(p.tlv[6:8]), 16)
-    print(binascii.b2a_hex(p.tlv))
     print(packet_id, ' ', packet_length, ' ', packet_seq)
     icn_type = p.payload[:2]
-    payload = p.payload[2:len(p.payload)]
+    payload = p.payload[34:len(p.payload)]
     DATA_SIZE_PER_UDP = UDP_MTU - p.header_len - len(icn_type)
     recv_times = 1
-    if not packet_id in temp_packet_dict.keys():
-        if packet_seq > top_n:
-            logger.warning('drop an unused packet')
-            return False
+    if packet_seq == 0:
+        temp_dict = {}
+        temp_data = binascii.a2b_hex('00' * packet_length)
+        temp_dict['data'] = payload + temp_data[len(payload):packet_length]
+        packet_num = packet_length / DATA_SIZE_PER_UDP
+        if packet_num == int(packet_num):
+            temp_dict['count'] = int(packet_num) * recv_times - recv_times + 1
         else:
-            temp_dict = {}
-            #temp_dict['icn_head'] = p
-            temp_data = binascii.a2b_hex('00' * packet_length)
-            temp_dict['data'] = temp_data[0:packet_seq * DATA_SIZE_PER_UDP] + payload + \
-                                temp_data[packet_seq * DATA_SIZE_PER_UDP + len(payload):packet_length]
-            packet_num = packet_length / DATA_SIZE_PER_UDP
-            if packet_num == int(packet_num):
-                temp_dict['count'] = int(packet_num) * recv_times
-            else:
-                temp_dict['count'] = (int(packet_num) + 1) * recv_times
-            #temp_dict['total'] = temp_dict['count']
-            temp_packet_dict[packet_id] = temp_dict
-            print(str(temp_packet_dict))
-            # logger.info('packet_seq: ' + str(packet_seq))
-            threading._start_new_thread(timeout_handler, (packet_id, address))
+            temp_dict['count'] = (int(packet_num) + 1) * recv_times - recv_times + 1
+        temp_dict['total'] = temp_dict['count']
+        p.setPayload(binascii.a2b_hex(''))
+        temp_dict['icn_head']=p
+        temp_packet_dict[packet_id] = temp_dict
+        print('packet_seq: ' + str(packet_seq))
+        threading._start_new_thread(timeout_handler, (packet_id, address))
     else:
-        temp_packet_dict[packet_id]['data'] = temp_packet_dict[packet_id]['data'][
-                                              0:packet_seq * DATA_SIZE_PER_UDP] + payload + \
-                                              temp_packet_dict[packet_id]['data'][
-                                              packet_seq * DATA_SIZE_PER_UDP + len(payload):packet_length]
-        # logger.info('packet_seq: ' + str(packet_seq))
+        if not packet_id in temp_packet_dict.keys():
+            return
+        else:
+            temp_data = temp_packet_dict[packet_id]['data'][0:packet_seq * DATA_SIZE_PER_UDP] + payload + \
+                        temp_packet_dict[packet_id]['data'][packet_seq * DATA_SIZE_PER_UDP + len(payload):packet_length]
+            temp_packet_dict[packet_id]['data'] = temp_data
+            print('packet_seq: ' + str(packet_seq))
     temp_packet_dict[packet_id]['count'] -= 1
-    logger.info('count: ' + str(temp_packet_dict[packet_id]['count']))
+    print('count: ' + str(temp_packet_dict[packet_id]['count']))
     if temp_packet_dict[packet_id]['count'] == 0:
-        data_finish_handler(temp_packet_dict[packet_id]['data'], address)
+        global ave_packet_recv_rate
+        ave_packet_recv_rate = 1
+        data_finish_handler(temp_packet_dict[packet_id]['icn_head'],temp_packet_dict[packet_id]['data'], address)
+        del temp_packet_dict[packet_id]
 
 
 def start_data_server(address):
